6fc2ea7348eaa5666c4b27179da9e7f8e286b661
lucasgcruz1 <130168665+lucasgcruz1@users.noreply.github.com>
2023-04-14 08:29:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,20 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
 
-// Rota para buscar os valores de luzes e velocidade
-app.get('/api/:luzes/:velocidade', (req, res) => {
-  const luzes = req.params.luzes === 'true'; // converte para booleano
-  const velocidade = parseInt(req.params.velocidade); // converte para inteiro
+// Configuração do middleware para lidar com dados em formato JSON
+app.use(express.json());
 
-  // Aqui você pode fazer o que quiser com os parâmetros luzes e velocidade,
-  // como por exemplo atualizar um arquivo JSON, fazer um cálculo, etc.
-
-  // Retornamos os valores em um objeto JSON
-  res.json({
-    luzes: luzes,
-    velocidade: velocidade
-  });
+// Rota para atualizar as luzes
+let lightsOn = false;
+app.post('/lights', (req, res) => {
+  lightsOn = req.body.value;
+  res.json({ success: true });
 });
 
-// Rota para editar o valor de luzes
-app.put('/api/luzes', (req, res) => {
-  // Lê o valor atual do arquivo JSON
-  const dados = JSON.parse(fs.readFileSync('dados.json'));
-
-  // Altera o valor de luzes para o valor enviado na requisição
-  dados.luzes = req.body.luzes === 'true'; // converte para booleano
-
-  // Salva o novo valor no arquivo JSON
-  fs.writeFileSync('dados.json', JSON.stringify(dados));
-
-  res.send('O valor de luzes foi atualizado.');
+// Rota para obter o status das luzes
+app.get('/lights', (req, res) => {
+  res.json({ lightsOn });
 });
 
-app.listen(3000, () => {
-  console.log('A API está rodando na porta 3000');
-});
+// Inicialização do servidor
+app.listen(3000, () => console.log('API rodando na porta 3000.'));
